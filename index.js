5a6840b117727bdef856f919cb576ce61bb74cd6
HotsixMan <hotsixman05@gmail.com>
2026-03-11 20:03:32 +0900

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+import { createTransformer } from './func/createTransformer.js'
+import { parseStream } from './func/parseStream.js'
+import { parse } from './func/parse.js';
+
+export default {
+    createTransformer,
+    parseStream,
+    parse
+}
